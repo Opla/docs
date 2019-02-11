bb4d73f8677f3ea9402a9bb649c4b763efed3c21
Damien CITAIRE <damien.citaire@gmail.com>
2019-02-11 17:14:28 +0100

    Integration - Change var to let (Arnaud feedback)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@
 ---
 // Toggle sidebar visibility
 function toggleSidebar() {
-    var element = document.getElementById("sidebar");
+    let element = document.getElementById("sidebar");
     element.classList.toggle("isVisible");
   }
